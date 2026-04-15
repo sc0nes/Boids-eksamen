0440a602745ce94824f93eeeae177df770f9cf71
Jonas Hansen <jonas@sundgaard.com>
2026-04-15 10:39:11 +0200

    Remove vector

--- Boid.java
+++ Boid.java
@@ -4,13 +4,9 @@ public class Boid {
     private BoidsLogic parent;
     int x;
     int y;
-    Vector<Double> pos = new Vector<Double>();
-    int[] posi = new int[3];
+    int[] pos = new int[3];
     public Boid(BoidsLogic p){
-        parent = p;
-        pos.set(0, 1d);
-        pos.set(1, 2d);
-        pos.add(2d);
+
 
     }
 }
